853183929f2c4a5a8cfa4638d2d9727c72b81a65
Mariusz Dąbrowski <mariuszd49@gmail.com>
2022-01-03 19:11:58 +0100

    User's software

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/user-software', [App\Http\Controllers\UserController::class, 'userSoftware'])->name('userSoftwares');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('/software', 'App\Http\Controllers\SoftwareController');
