e4be3ead71a50e4669417f8023b5685815bd96d0
Tomasz <tomekoleszko@gmail.com>
2022-01-03 18:54:18 +0100

    create fix

--- resources/views/Softwares/create.blade.php
+++ resources/views/Softwares/create.blade.php
@@ -18,34 +18,56 @@
         </div>
     @endif
     @auth
-        <div class="container">
-            <form role="form" action="{{route('software.store')}}" method="post">
+        <div class="container align-content-center">
+            <form role="form" action="{{route('software.store')}}" method="post" class="row g-3 align-items-center needs-validation w-90" novalidate>
                 @csrf
 
-                <div class="mb-3">
-                    <label class="form-label">Title</label>
-                    <input name="title" id="title"/>
+                <div class="col-md-6 mb-3 mt-3">
+                    <label for="title" class="form-label">Title</label>
+                    <input type="text" class="form-control" id="title" placeholder="Enter Title" name="title" required>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="link" class="form-label">Link:</label>
+                    <input type="url" class="form-control" id="link" placeholder="Enter url" name="link" required>
                 </div>
                 <div class="mb-3">
-                    <label class="form-label">Description</label>
-                    <input name="description" id="desc"/>
+                    <label for="description">Description:</label>
+                    <textarea class="form-control" rows="5" id="desc" name="description" required></textarea>
                 </div>
+
                 <div class="mb-3">
-                    <label class="form-label">Link</label>
-                    <input name="link" id="link"/>
-                </div>
-                <div class="mb-3">
-                    <label for="category">Icon:</label>
-                    <select name="licence" id="categories">
-                        <option value="apache2">Apache License 2.0 (Apache-2.0)</option>
+                    <select class="form-select " name="licence" id="categories" aria-label="Default select example">
+                        <option selected value="apache2">Apache License 2.0 (Apache-2.0)</option>
                         <option value="GNU3">GNU General Public License v3.0</option>
                         <option value="MIT">MIT License</option>
                         <option value="CCZ">Creative Commons Zero v1.0 Universal</option>
                     </select>
                 </div>
-                <button type="submit" class="btn btn-primary" >Submit</button>
-            </form>
 
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
+        <script>
+            (function () {
+                'use strict'
+
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.querySelectorAll('.needs-validation')
+
+                // Loop over them and prevent submission
+                Array.prototype.slice.call(forms)
+                    .forEach(function (form) {
+                        form.addEventListener('submit', function (event) {
+                            if (!form.checkValidity()) {
+                                event.preventDefault()
+                                event.stopPropagation()
+                            }
+
+                            form.classList.add('was-validated')
+                        }, false)
+                    })
+            })()
+        </script>
+
     @endauth
 @endsection
