0a3b59beeb854960994abd50e112c103b4d26e7b
Mariusz Dąbrowski <mariuszd49@gmail.com>
2022-01-05 23:36:48 +0100

    add software repository class

---
+++ app/Repositories/SoftwareRepository.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Repositories;
+use Illuminate\Support\Facades\DB;
+use App\Models\Software;
+
+class SoftwareRepository extends BaseRepository{
+    public function __construct(Software $model)
+    {
+        $this->model = $model;
+    }
+
+    public function getByLicence($licence)
+    {
+        if($licence){
+            $softwareList = $this->model->where('licence', '=', $licence)->paginate(10);
+        }
+        else $softwareList = $this->paginate();
+
+        return $softwareList;
+    }
+}
+
+?>
